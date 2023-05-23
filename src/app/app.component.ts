0695d9a3e979f5e0bb114691148e32d2bb86a2cf
Allan <j.a.o.huthan@gmail.com>
2023-05-23 07:55:38 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics-module';
+  userName!: string;
+  serverStatus = 'offline';
+
+  onClick() {
+    this.userName = '';
+    this.serverStatus = 'online';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
